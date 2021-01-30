d913904ef25624c852ba6f18fc20dff9e6fd34a8
Balazs <maczak.balazs@gmail.com>
2021-01-30 21:46:19 +0100

    asist

---
+++ Assets/Scripts/AssemblyAssist.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class AssemblyAssist : MonoBehaviour
+{
+    [System.Serializable]
+    public struct AssistSetting
+    {
+        public float maxTorque;
+        public AnimationCurve animCurve;
+    }
+
+    [SerializeField] AssistSetting setting = new AssistSetting();
+
+    Rigidbody2D rb;
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    void FixedUpdate()
+    {
+        float angle = transform.eulerAngles.z;
+        
+        while (angle > 180f)
+        {
+            angle -= 360f;
+        }
+        
+        while (angle < -180f)
+        {
+            angle += 360f;
+        }
+
+        rb.AddTorque(-setting.animCurve.Evaluate(Mathf.Abs(angle / 180f)) * setting.maxTorque * Mathf.Sign(angle));
+    }
+}
